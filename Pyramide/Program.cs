27fe9e8bd84ad63ddbfe5d78dd57d75b6993f0b7
Alexandr <alexraiko2211@gmail.com>
2021-05-17 17:50:47 +0300

    Add project files.

---
+++ Pyramide/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Pyramide
+{
+    class Pyramide
+    {
+        static void Main(string[] args)
+        {
+            string[] row1 = { " ", " ", " ", " ", " ", " ", " ", "*" };
+            string[] row2 = { " ", " ", " ", " ", " ", " ", "*", "*", "*" };
+            string[] row3 = { " ", " ", " ", " ", " ", "*", "*", "*", "*", "*" };
+            string[] row4 = { " ", " ", " ", " ", "*", "*", "*", "*", "*", "*", "*" };
+            string[] row5 = { " ", " ", " ", "*", "*", "*", "*", "*", "*", "*", "*", "*" };
+            string[] row6 = { " ", " ", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*" };
+            string[] row7 = { " ", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*", "*" };
+            string[][] pyramide = { row1, row2, row3, row4, row5, row6, row7 };
+
+
+            for (int i = 0; i < pyramide.Length; i++)
+            {
+                for (int j = 0; j < pyramide[i].Length; j++)
+                {
+                    Console.Write(pyramide[i][j].ToString() + " ");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
